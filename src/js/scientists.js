c03aac7a4cccac089dfd121a6ba2ecbb56021f1d
Matvii-Alkhimov <112165072+Matvii-Alkhimov@users.noreply.github.com>
2023-07-29 15:23:29 +0300

    Update scientists.js

--- src/js/scientists.js
+++ src/js/scientists.js
@@ -1,24 +1,24 @@
-import AdaLovelace from '../images_and_icons/images/scientists/Ada_Lovelace.jpg';
-import AlbertEinstein from '../images_and_icons/images/scientists/Albert_Einstein.jpg';
-import GalileoGalilei from '../images_and_icons/images/scientists/Galileo_Galilei.jpg';
-import HannaHammarström from '../images_and_icons/images/scientists/Hanna_Hammarström.jpg';
-import IsaacNewton from '../images_and_icons/images/scientists/Isaac_Newton.jpg';
-import JohannesKepler from '../images_and_icons/images/scientists/Johannes_Kepler.jpg';
-import KatherineBlodgett from '../images_and_icons/images/scientists/Katherine_Blodgett.jpg';
-import LiseMeitner from '../images_and_icons/images/scientists/Lise_Meitner.jpg';
-import MarieCurie from '../images_and_icons/images/scientists/Marie_Curie.jpg';
-import MaxPlanck from '../images_and_icons/images/scientists/Max_Planck.jpg';
-import NikolausCopernikus from '../images_and_icons/images/scientists/Nikolaus_Copernikus.jpg';
-import SarahGoode from '../images_and_icons/images/scientists/Sarah_Goode.jpg';
+// import AdaLovelace from '../images_and_icons/images/scientists/Ada_Lovelace.jpg';
+// import AlbertEinstein from '../images_and_icons/images/scientists/Albert_Einstein.jpg';
+// import GalileoGalilei from '../images_and_icons/images/scientists/Galileo_Galilei.jpg';
+// import HannaHammarström from '../images_and_icons/images/scientists/Hanna_Hammarström.jpg';
+// import IsaacNewton from '../images_and_icons/images/scientists/Isaac_Newton.jpg';
+// import JohannesKepler from '../images_and_icons/images/scientists/Johannes_Kepler.jpg';
+// import KatherineBlodgett from '../images_and_icons/images/scientists/Katherine_Blodgett.jpg';
+// import LiseMeitner from '../images_and_icons/images/scientists/Lise_Meitner.jpg';
+// import MarieCurie from '../images_and_icons/images/scientists/Marie_Curie.jpg';
+// import MaxPlanck from '../images_and_icons/images/scientists/Max_Planck.jpg';
+// import NikolausCopernikus from '../images_and_icons/images/scientists/Nikolaus_Copernikus.jpg';
+// import SarahGoode from '../images_and_icons/images/scientists/Sarah_Goode.jpg';
 import scientists from '../arrays/scientists.json';
 console.log(scientists)
 
-const imagesArr = [
-  AlbertEinstein, IsaacNewton, GalileoGalilei,
-  MarieCurie, JohannesKepler, NikolausCopernikus,
-  MaxPlanck, KatherineBlodgett, AdaLovelace,
-  SarahGoode, LiseMeitner, HannaHammarström
-];
+// const imagesArr = [
+//   AlbertEinstein, IsaacNewton, GalileoGalilei,
+//   MarieCurie, JohannesKepler, NikolausCopernikus,
+//   MaxPlanck, KatherineBlodgett, AdaLovelace,
+//   SarahGoode, LiseMeitner, HannaHammarström
+// ];
 
 
 // const scientists = [ 
